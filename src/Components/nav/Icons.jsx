bd18c9de278b4c005c67e657d8a71e6ab6eb6f42
Baptiste Ribot <baptisteribot@outlook.fr>
2023-07-09 14:54:12 +0200

    nav begin

---
+++ src/Components/nav/Icons.jsx
@@ -0,0 +1,20 @@
+function Icons() {
+  return (
+    <ul className="ul-nav">
+      <li>
+        <i className="fa-solid fa-house not-colored"></i>
+      </li>
+      <li>
+        <i className="fa-solid fa-briefcase not-colored"></i>
+      </li>
+      <li>
+        <i className="fa-solid fa-comment-dots not-colored"></i>
+      </li>
+      <li>
+        <i className="fa-solid fa-chart-simple not-colored"></i>
+      </li>
+    </ul>
+  );
+}
+
+export default Icons;
--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -1,10 +1,13 @@
 import "./nav.css";
+import Icons from "./Icons";
 
 function Nav() {
   return (
-    <div>
-      <img src="../images/picture.png" alt="Baptiste Ribot" />
-      <div></div>
+    <div className="div-nav">
+      <img src="../images/picture.png" alt="Baptiste Ribot" className="logo" />
+      <div className="nav-bar">
+        <Icons />
+      </div>
     </div>
   );
 }
