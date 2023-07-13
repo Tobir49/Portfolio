15ce39da60233e6eaeecfff1c09bc0110baf51e8
Baptiste Ribot <baptisteribot@outlook.fr>
2023-07-13 17:44:37 +0200

    me

---
+++ src/Components/Me/Me.jsx
@@ -0,0 +1,35 @@
+function Me() {
+  return (
+    <div>
+      <div>
+        <h2>BIO</h2>
+        <p>
+          Jeune <span>développeur</span> de 19 ans, ayant découvert ce domaine
+          fin 2022.
+        </p>
+        <p>
+          Ayant suivi une formation de <span>Développeur Web</span>
+          sur <span>Openclassrooms</span> de décembre 2022 à août 2023, j’ai
+          acquis de nombreuses compétences (langages et frameworks) et su
+          utiliser certains outils, comme Figma, GitHub ou bien Postman.
+        </p>
+      </div>
+      <div>
+        <h2>HOBBIES</h2>
+        <p>
+          Amateur de <span>lecture</span>, je suis surtout tourné vers les
+          <span>mangas</span>. Les collectionner sont ma plus grande passion
+          (plus de 700 actuellement). J’aime partager ce hobby en créant du
+          contenu divertissant sur diverses plateformes.
+        </p>
+        <p>
+          En plus de mes autres passions, j’adore me <span>balader</span>,
+          <span>profiter</span> de mes proches et <span>jouer</span> avec eux
+          (jeux de sociétés / vidéos).
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Me;
--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import "./home.css";
 import Header from "../../Components/Header/Header";
 import Nav from "../../Components/Nav/Nav";
+import Me from "../../Components/Me/Me";
 
 function Home() {
   return (
     <div>
       <Nav />
       <Header />
+      <Me />
     </div>
   );
 }
