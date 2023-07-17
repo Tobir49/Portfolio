c87c2996f747df74941ec271c597f2774d8c1493
Baptiste Ribot <baptisteribot@outlook.fr>
2023-07-17 09:33:19 +0200

    projects & contact v2

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,31 +1,44 @@
+import "./contact.css";
+
 function Contact() {
   return (
-    <article id="contact">
-      <div>
-        <i className="fa-solid fa-location-dot yellow"></i>
-        <p className="white">1, rue Pierre Nion. Trélazé 49800</p>
+    <article id="contact" className="contact">
+      <div className="div-one-contact">
+        <i className="fa-solid fa-location-dot yellow contact-icon"></i>
+        <div className="contact-location">
+          <p className="white contact-text">1, rue Pierre Nion.</p>
+          <p className="white contact-text">Trélazé 49800</p>
+        </div>
       </div>
-      <div>
-        <i className="fa-solid fa-envelope yellow"></i>
-        <p className="white">baptisteribot@outlook.fr</p>
+
+      <div className="div-one-contact">
+        <i className="fa-solid fa-envelope yellow contact-icon"></i>
+        <p className="white contact-text">baptisteribot@outlook.fr</p>
       </div>
-      <div>
-        <i className="fa-solid fa-mobile yellow"></i>
-        <p className="white">06.89.97.85.41</p>
+
+      <div className="div-one-contact">
+        <i className="fa-solid fa-mobile yellow contact-icon"></i>
+        <p className="white contact-text">06.89.97.85.41</p>
       </div>
-      <div>
-        <i className="fa-brands fa-github yellow"></i>
-        <a href="https://github.com/Tobir49" target="blank" className="white">
+
+      <div className="div-one-contact">
+        <a
+          href="https://github.com/Tobir49"
+          target="blank"
+          className="white contact-a"
+        >
+          <i className="fa-brands fa-github yellow contact-icon"></i>
           GitHub
         </a>
       </div>
-      <div>
-        <i className="fa-brands fa-linkedin yellow"></i>
+
+      <div className="div-one-contact">
         <a
           href="https://linkedin.com/in/baptiste-ribot-6bb910229"
           target="blank"
-          className="white"
+          className="white contact-a"
         >
+          <i className="fa-brands fa-linkedin yellow contact-icon"></i>
           Linkedin
         </a>
       </div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -46,13 +46,14 @@ function Projects() {
             className="pictures-projects"
           />
           <div>
-            <p className="white">⏰ 01-2022</p>
-            <p className="white">🪪 BOOKI</p>
+            <p className="white">⏰ 02/03-2023</p>
+            <p className="white">🪪 Sophie Buel - Architecte</p>
             <p className="white center text-project">
-              💻 Intégration d’un contenu web statique à partir d’une maquette
-              Figma. Le site devait être responsive sur : Ordinateur Tablette
-              Mobile. Première utilisation de GitHub sur ce projet, ainsi que de
-              GitHub Pages pour rendre le site accessible à tous.
+              💻 Création d’un code JavaScript (pas de frameworks) afin de
+              rendre le site d’une architecte dynamique. Utilisation d’un token,
+              création d’une fonction login, création d’une galerie de projet à
+              partir d’une base de données. Gestion des routes grâce à Postman &
+              Swagger.
             </p>
           </div>
         </div>
@@ -64,13 +65,14 @@ function Projects() {
             className="pictures-projects"
           />
           <div>
-            <p className="white">⏰ 01-2022</p>
-            <p className="white">🪪 BOOKI</p>
+            <p className="white">⏰ 04-2023</p>
+            <p className="white">🪪 Gestion du développement d'un site</p>
             <p className="white center text-project">
-              💻 Intégration d’un contenu web statique à partir d’une maquette
-              Figma. Le site devait être responsive sur : Ordinateur Tablette
-              Mobile. Première utilisation de GitHub sur ce projet, ainsi que de
-              GitHub Pages pour rendre le site accessible à tous.
+              💻 Afin de gérer le bon développement d’un site, j’ai dû créer le
+              document des spécifications techniques, gérer les étapes de chaque
+              sprint, créer des user stories en prenant en compte les
+              technologies à utiliser et présenter cela au chef de projet du
+              client.
             </p>
           </div>
         </div>
@@ -82,13 +84,12 @@ function Projects() {
             className="pictures-projects"
           />
           <div>
-            <p className="white">⏰ 01-2022</p>
-            <p className="white">🪪 BOOKI</p>
+            <p className="white">⏰ 04/05-2023</p>
+            <p className="white">🪪 Debug et Optimisation d'un site</p>
             <p className="white center text-project">
-              💻 Intégration d’un contenu web statique à partir d’une maquette
-              Figma. Le site devait être responsive sur : Ordinateur Tablette
-              Mobile. Première utilisation de GitHub sur ce projet, ainsi que de
-              GitHub Pages pour rendre le site accessible à tous.
+              💻 Avec l’aide des Chrome Devtools, il fallait rendre un site de
+              photographe plus accessible, améliorer ses performances ainsi que
+              SEO. L’utilisation de Javascript était nécessaire.
             </p>
           </div>
         </div>
@@ -100,13 +101,13 @@ function Projects() {
             className="pictures-projects"
           />
           <div>
-            <p className="white">⏰ 01-2022</p>
+            <p className="white">⏰ 05-2023</p>
             <p className="white">🪪 BOOKI</p>
             <p className="white center text-project">
-              💻 Intégration d’un contenu web statique à partir d’une maquette
-              Figma. Le site devait être responsive sur : Ordinateur Tablette
-              Mobile. Première utilisation de GitHub sur ce projet, ainsi que de
-              GitHub Pages pour rendre le site accessible à tous.
+              💻 Création d’une application React à partir d’une maquette.
+              Création d’un dossier ordonné et trié selon les composants et les
+              pages. Logique de routage, ré-utilisation des composants (props,
+              hooks) et css ordonné.
             </p>
           </div>
         </div>
@@ -114,17 +115,17 @@ function Projects() {
         <div className="div-project">
           <img
             src={projectSix}
-            alt="projet Bakc-End"
+            alt="projet Back-End"
             className="pictures-projects"
           />
           <div>
-            <p className="white">⏰ 01-2022</p>
-            <p className="white">🪪 BOOKI</p>
+            <p className="white">⏰ 06/07-2023</p>
+            <p className="white">🪪 Back-End d'un site</p>
             <p className="white center text-project">
-              💻 Intégration d’un contenu web statique à partir d’une maquette
-              Figma. Le site devait être responsive sur : Ordinateur Tablette
-              Mobile. Première utilisation de GitHub sur ce projet, ainsi que de
-              GitHub Pages pour rendre le site accessible à tous.
+              💻 Clonage d’un code Front-End afin d’en créer le Back dans le but
+              de rendre un site de notation de livres dynamique. Utilisation
+              d’une base données MongoDB (Atlas), création d’un serveur Node et
+              d’une API Express.
             </p>
           </div>
         </div>
