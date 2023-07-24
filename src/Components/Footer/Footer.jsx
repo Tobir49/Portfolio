946a143af4eb29308800159275e14f3c4d57046f
Baptiste Ribot <baptisteribot@outlook.fr>
2023-07-24 16:00:25 +0200

    i hover

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,16 +10,16 @@ function Footer() {
             href="https://www.figma.com/file/dYB14VWhEs610tAqB1686H/Maquette?type=design&node-id=201%3A6&mode=design&t=uhyhNG300AWhVltA-1"
             target="blank"
           >
-            <i className="fa-brands fa-figma white"></i>
+            <i className="fa-brands fa-figma white footer-icon"></i>
           </a>
           <a href="https://github.com/Tobir49" target="blank">
-            <i className="fa-brands fa-github white"></i>
+            <i className="fa-brands fa-github white footer-icon"></i>
           </a>
           <a
             href="https://linkedin.com/in/baptiste-ribot-6bb910229"
             target="blank"
           >
-            <i className="fa-brands fa-linkedin white"></i>
+            <i className="fa-brands fa-linkedin white footer-icon"></i>
           </a>
         </div>
       </div>
