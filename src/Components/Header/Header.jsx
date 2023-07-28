5b65941b297425a042f7b721f5bbf229c1b89509
Baptiste Ribot <baptisteribot@outlook.fr>
2023-07-28 10:04:25 +0200

    ajout classes flex-direction

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@ import "./header.css";
 
 function Header() {
   return (
-    <header>
+    <header className="column">
       <div>
         <h1 className="white">RIBOT Baptiste</h1>
         <h2 className="h2-header white basic">💻 Développeur Web 💻</h2>
