b84c5aeb3119d583d01633f0855d04828544df9a
Baptiste Ribot <baptisteribot@outlook.fr>
2023-07-31 10:26:57 +0200

    mediaqueries skills

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -6,12 +6,12 @@ function Skills() {
   return (
     <article id="skills" className="z-index">
       <div className="div-web">
-        <h2 className="blue bold">Web :</h2>
+        <h2 className="blue bold title-skills">Web :</h2>
         <WebIcons />
       </div>
       <br />
       <div className="div-others">
-        <h2 className="blue bold">Autres :</h2>
+        <h2 className="blue bold title-skills">Autres :</h2>
         <OtherIcons />
       </div>
     </article>
