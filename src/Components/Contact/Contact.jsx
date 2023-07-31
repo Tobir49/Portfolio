296ff45a6aa92252d02ea38ae8d0b3f1a088e120
Baptiste Ribot <baptisteribot@outlook.fr>
2023-07-31 14:47:57 +0200

    css last details

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,27 +4,27 @@ function Contact() {
   return (
     <article id="contact" className="contact z-index column justify">
       <div className="div-one-contact">
-        <i className="fa-solid fa-location-dot yellow contact-icon"></i>
-        <p className="white contact-text">1, rue Pierre Nion. Trélazé 49800</p>
+        <i className="fa-solid fa-location-dot blue contact-icon"></i>
+        <p className="blue contact-text">1, rue Pierre Nion. Trélazé 49800</p>
       </div>
 
       <div className="div-one-contact">
-        <i className="fa-solid fa-envelope yellow contact-icon"></i>
-        <p className="white contact-text">baptisteribot@outlook.fr</p>
+        <i className="fa-solid fa-envelope blue contact-icon"></i>
+        <p className="blue contact-text">baptisteribot@outlook.fr</p>
       </div>
 
       <div className="div-one-contact">
-        <i className="fa-solid fa-mobile yellow contact-icon"></i>
-        <p className="white contact-text">06.89.97.85.41</p>
+        <i className="fa-solid fa-mobile blue contact-icon"></i>
+        <p className="blue contact-text">06.89.97.85.41</p>
       </div>
 
       <div className="div-one-contact">
         <a
           href="https://github.com/Tobir49"
           target="blank"
-          className="white contact-a"
+          className="blue contact-a"
         >
-          <i className="fa-brands fa-github yellow contact-icon"></i>
+          <i className="fa-brands fa-github blue contact-icon"></i>
           GitHub
         </a>
       </div>
@@ -33,9 +33,9 @@ function Contact() {
         <a
           href="https://linkedin.com/in/baptiste-ribot-6bb910229"
           target="blank"
-          className="white contact-a"
+          className="blue contact-a"
         >
-          <i className="fa-brands fa-linkedin yellow contact-icon"></i>
+          <i className="fa-brands fa-linkedin blue contact-icon"></i>
           Linkedin
         </a>
       </div>
