f358319646764e83c93c166115a4e4d7edb54ac0
Baptiste Ribot <baptisteribot@outlook.fr>
2023-08-22 13:53:14 +0200

    seo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,10 @@ function Header() {
   return (
     <header className="column align-items justify">
       <div>
-        <h1 className="white">RIBOT Baptiste</h1>
+        <h1 className="white">
+          {" "}
+          <span itemProp={"name"}>RIBOT Baptiste</span>
+        </h1>
         <h2 className="h2-header white basic">💻 Développeur Web 💻</h2>
       </div>
       <a href="#projects" className="a-header yellow">
