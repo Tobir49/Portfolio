860843246ea13dc4f8fd9e7b2953a2d96c22ffce
Baptiste Ribot <baptisteribot@outlook.fr>
2023-09-06 12:06:24 +0200

    changement couleurs

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,22 +4,22 @@ function Footer() {
   return (
     <footer className="z-index">
       <div className="copyright align-items">
-        <p className="blue footer-text">©2023 - Tous droits réservés</p>
+        <p className="white footer-text">©2023 - Tous droits réservés</p>
         <div>
           <a
             href="https://www.figma.com/file/dYB14VWhEs610tAqB1686H/Maquette?type=design&node-id=201%3A6&mode=design&t=uhyhNG300AWhVltA-1"
             target="blank"
           >
-            <i className="fa-brands fa-figma blue footer-icon"></i>
+            <i className="fa-brands fa-figma white footer-icon"></i>
           </a>
           <a href="https://github.com/Tobir49" target="blank">
-            <i className="fa-brands fa-github blue footer-icon"></i>
+            <i className="fa-brands fa-github white footer-icon"></i>
           </a>
           <a
             href="https://linkedin.com/in/baptiste-ribot-6bb910229"
             target="blank"
           >
-            <i className="fa-brands fa-linkedin blue footer-icon"></i>
+            <i className="fa-brands fa-linkedin white footer-icon"></i>
           </a>
         </div>
       </div>
--- src/Components/Me/Me.jsx
+++ src/Components/Me/Me.jsx
@@ -1,4 +1,4 @@
-import picture from "../../../images/picture.png";
+import picture from "../../../images/icon.png";
 import "./me.css";
 
 function Me() {
@@ -7,63 +7,63 @@ function Me() {
       <img src={picture} alt="image Ribot Baptiste" className="picture-me" />
       <div className="text-main">
         <div className="biopic">
-          <h2 className="blue border">BIO</h2>
-          <p className="blue center">
-            Jeune <span className="blue bold">développeur</span> de 19 ans,
+          <h2 className="white border">BIO</h2>
+          <p className="white center">
+            Jeune <span className="white bold">développeur</span> de 19 ans,
             ayant découvert ce domaine fin 2022, à la suite d'un
-            <span className="blue bold"> changement d'orientation</span>.
+            <span className="white bold"> changement d'orientation</span>.
           </p>
-          <p className="blue center">
+          <p className="white center">
             J'ai suivi une formation de
-            <span className="blue bold"> Développeur Web </span>
-            sur <span className="blue bold">Openclassrooms</span> de décembre
+            <span className="white bold"> Développeur Web </span>
+            sur <span className="white bold">Openclassrooms</span> de décembre
             2022 à août 2023, et acquis de nombreuses compétences dans le
             domaine du web (langages ou frameworks). De plus j'ai appris à
             utiliser certains outils, comme Figma, GitHub ou bien Postman.
           </p>
-          <p className="blue center">
+          <p className="white center">
             Finalement, cette formation m'aura aussi permis d'acquérir des soft
-            skills comme l'<span className="blue bold">autonomie</span> et de la{" "}
-            <span className="blue bold">flexibilité.</span> Hormis ceux-ci, j'ai
-            un bon <span className="blue bold">esprit</span> et une bonne{" "}
-            <span className="blue bold">gestion d'équipe</span>, suis{" "}
-            <span className="blue bold">sociable</span> et je{" "}
-            <span className="blue bold">communique </span>
+            skills comme l'<span className="white bold">autonomie</span> et de
+            la <span className="white bold">flexibilité.</span> Hormis ceux-ci,
+            j'ai un bon <span className="white bold">esprit</span> et une bonne{" "}
+            <span className="white bold">gestion d'équipe</span>, suis{" "}
+            <span className="white bold">sociable</span> et je{" "}
+            <span className="white bold">communique </span>
             facilement et agréablement. Pour plus de détails sur mes skills,
             cliquez{" "}
-            <a href="#skills" className="blue border bold">
+            <a href="#skills" className="white border bold">
               ici
             </a>
             .
           </p>
-          <p className="blue center">
+          <p className="white center">
             Si vous souhaitez en savoir plus sur moi je vous invite à découvrir
             les{" "}
-            <a href="#projects" className="blue border bold">
+            <a href="#projects" className="white border bold">
               projets
             </a>{" "}
             que j'ai menés, ou bien à me{" "}
-            <a href="#contact" className="blue border bold">
+            <a href="#contact" className="white border bold">
               contacter
             </a>{" "}
             !
           </p>
         </div>
         <div className="hobbies">
-          <h2 className="blue border">HOBBIES</h2>
-          <p className="blue center">
-            Amateur de <span className="blue bold">lecture</span>, je suis
+          <h2 className="white border">HOBBIES</h2>
+          <p className="white center">
+            Amateur de <span className="white bold">lecture</span>, je suis
             surtout tourné vers les
-            <span className="blue bold"> mangas</span>. Les collectionner est ma
-            plus grande passion (plus de 700 actuellement). J’aime partager ce
-            hobby en créant du contenu divertissant sur diverses plateformes.
+            <span className="white bold"> mangas</span>. Les collectionner est
+            ma plus grande passion (plus de 700 actuellement). J’aime partager
+            ce hobby en créant du contenu divertissant sur diverses plateformes.
           </p>
-          <p className="blue center">
+          <p className="white center">
             En plus de mes autres passions, j’adore me
-            <span className="blue bold"> balader</span>,
-            <span className="blue bold"> profiter</span> de mes proches et
-            <span className="blue bold"> jouer</span> avec eux (jeux de sociétés
-            / vidéos).
+            <span className="white bold"> balader</span>,
+            <span className="white bold"> profiter</span> de mes proches et
+            <span className="white bold"> jouer</span> avec eux (jeux de
+            sociétés / vidéos).
           </p>
         </div>
       </div>
